2d6c3c720a3f7770f69f44cefafe7c2be549a686
Christian Landgren <christian@landgren.nu>
2022-05-22 09:01:30 +0200

    feat: generalized api

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -3,23 +3,23 @@ const actor = require('../lib/actor.js')
 test('actor can not read parcel destination before being added as reader', async () => {
   expect.assertions(1)
 
-  const zalando = await actor.create('Zalando')
-  const postnord = await actor.create('Postnord')
-  const parcel = await zalando.createParcel()
+  const zalando = await actor.create('Zalando', 'parcels')
+  const postnord = await actor.create('Postnord', 'parcels')
+  const parcel = await zalando.createObject({ pickup: 'Zalando' })
 
-  await expect(postnord.receiveParcel(parcel.id))
+  await expect(postnord.readPayload(parcel.id))
     .rejects.toMatchObject(new Error('no key found'))
 })
 
 test('actor can receive parcel after being added as reader', async () => {
   expect.assertions(1)
 
-  const zalando = await actor.create('Zalando')
-  const postnord = await actor.create('Postnord')
-  const parcel = await zalando.createParcel()
+  const zalando = await actor.create('Zalando', 'parcels')
+  const postnord = await actor.create('Postnord', 'parcels')
+  const parcel = await zalando.createObject({ pickup: 'Zalando' })
 
-  await zalando.addDestinationReader(parcel.id, postnord)
+  await zalando.addPayloadReader(parcel.id, postnord)
 
-  await expect(postnord.receiveParcel(parcel.id))
+  await expect(postnord.readPayload(parcel.id))
     .resolves.toBeDefined()
 })
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 (async () => {
   const actor = require('./lib/actor.js')
 
-  let zalando = await actor.create('Zalando')
-  let postnord = await actor.create('Postnord')
+  let zalando = await actor.create('Zalando', 'parcels')
+  let postnord = await actor.create('Postnord', 'parcels')
 
   let package = await zalando.createParcel()
-  await zalando.addDestinationReader(package.id, postnord)
+  await zalando.addPayloadReader(package.id, postnord)
   const updatedPackage = await postnord.receiveParcel(package.id)
   console.log('package', updatedPackage)
 })()
