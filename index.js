b26d8af054448b027718f72aa6b6fb8aaa7ef7ab
Christian Landgren <christian@landgren.nu>
2022-05-22 09:03:32 +0200

    chore: update examples

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
   let zalando = await actor.create('Zalando', 'parcels')
   let postnord = await actor.create('Postnord', 'parcels')
 
-  let package = await zalando.createParcel()
+  let package = await zalando.createObject({ pickup: 'Zalando' })
   await zalando.addPayloadReader(package.id, postnord)
-  const updatedPackage = await postnord.receiveParcel(package.id)
+  const updatedPackage = await postnord.readPayload(package.id)
   console.log('package', updatedPackage)
 })()
